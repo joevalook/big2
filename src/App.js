11f99f02649e60512163e43b9005f063174126c2
Joe Valookaran <joevalookaran@hotmail.com>
2022-09-06 17:48:01 -0400

    added props from main app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import Player1 from "./components/Player1";
-import Player2 from "./components/Player1";
-import Player3 from "./components/Player1";
-import Player4 from "./components/Player1";
+import PlayerScore from "./components/PlayerScore";
 import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [position, setPosition] = useState('first');
+  const [player1, setPlayer1] = useState(0);
+  const [player2, setPlayer2] = useState(0);
+  const [player3, setPlayer3] = useState(0);
+  const [player4, setPlayer4] = useState(0);
   return (
     <div className={`App`}>
       <h1>Big 2 Score Keeping</h1>
+      <h2>Who came {position}?</h2>
       <section>
-        <Player1 />
-        <Player2 />
-        <Player3 />
-        <Player4 />
+        <PlayerScore player = {player1} setPlayer = {setPlayer1} position = {position} setPosition={setPosition}/>
+        <PlayerScore player = {player2} setPlayer = {setPlayer2} position = {position} setPosition={setPosition}/>
+        <PlayerScore player = {player3} setPlayer = {setPlayer3} position = {position} setPosition={setPosition}/>
+        <PlayerScore player = {player4} setPlayer = {setPlayer4} position = {position} setPosition={setPosition}/>
       </section>
     </div>
   );
